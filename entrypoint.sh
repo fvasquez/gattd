fca0a0585a4b708a14cbcd6bb2c0a40d6b5f3354
Frank Vasquez <frank.vasquez@gmail.com>
2025-01-16 22:00:09 -0800

    Wait for bluez service to start

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,17 @@
 service dbus start
 service bluetooth start
 
+# Wait for services to start
+msg="Waiting for services to start..."
+time=0
+echo -n $msg
+while [[ "$(pidof start-stop-daemon)" != "" ]]; do
+    sleep 1
+    time=$((time + 1))
+    echo -en "\r$msg $time s"
+done
+echo -e "\r$msg done! (in $time s)"
+
 # Reset Bluetooth adapter by restarting it
 hciconfig hci0 down
 hciconfig hci0 up
